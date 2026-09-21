1068af7467a90f158a47f4796ec5896664b40090
Min Young Bang <andrewscis7@gmail.com>
2026-09-21 20:34:48 +0900

    feat: sync latest local version (components/avatar)

--- components/avatar/Avatar.tsx
+++ components/avatar/Avatar.tsx
@@ -5,8 +5,8 @@ export default function Avatar() {
   return (
     <svg className="common-avatar" viewBox="0 0 32 32" aria-hidden="true">
       <circle cx="16" cy="16" r="16" fill="#ededed" />
-      <circle cx="16" cy="11" r="6" fill="#909090" />
-      <path d="M4 29c0-8 5-12 12-12s12 4 12 12" fill="#909090" />
+      <circle cx="16" cy="11.5" r="4.5" fill="#909090" />
+      <path d="M8 25.5c0-4.2 3.6-7 8-7s8 2.8 8 7c0 .8-.7 1.5-1.5 1.5h-13c-.8 0-1.5-.7-1.5-1.5z" fill="#909090" />
     </svg>
   );
 }
